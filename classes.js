8c7b6a5b497c5ef923435dc55dc898b32f09ad02
Stephen McMaster <stephen@ModularThinkingLimited.onmicrosoft.com>
2022-04-16 04:08:12 +0100

    Adding back .ts files

--- classes.js
+++ classes.js
@@ -1,48 +1,44 @@
 "use strict";
-exports.__esModule = true;
-exports.Attributes = exports.SubnetSettings = exports.Subnet = exports.Response = exports.SubnetType = void 0;
+Object.defineProperty(exports, "__esModule", { value: true });
 var SubnetType;
 (function (SubnetType) {
     SubnetType[SubnetType["Private"] = 0] = "Private";
     SubnetType[SubnetType["Public"] = 1] = "Public";
 })(SubnetType = exports.SubnetType || (exports.SubnetType = {}));
-var Response = /** @class */ (function () {
-    function Response() {
+class Response {
+    constructor() {
         this.AdditionalCidrs = [];
         this.Subnets = [];
     }
-    return Response;
-}());
+}
 exports.Response = Response;
-var Subnet = /** @class */ (function () {
-    function Subnet(init) {
+class Subnet {
+    constructor(init) {
         this.name = "";
         this.cidr = "";
         this.type = SubnetType.Private;
         Object.assign(this, init);
     }
-    return Subnet;
-}());
+}
 exports.Subnet = Subnet;
-var SubnetSettings = /** @class */ (function () {
-    function SubnetSettings(init) {
+class SubnetSettings {
+    constructor(init) {
         this.cidr = "";
         this.subnets = [];
         this.count = 3;
         this.prefix = "";
         Object.assign(this, init);
     }
-    SubnetSettings.prototype.mask = function () {
+    mask() {
         return Number(this.cidr.split("/")[1]);
-    };
-    SubnetSettings.prototype.ip = function () {
+    }
+    ip() {
         return this.cidr.split("/")[0];
-    };
-    return SubnetSettings;
-}());
+    }
+}
 exports.SubnetSettings = SubnetSettings;
-var Attributes = /** @class */ (function () {
-    function Attributes(init) {
+class Attributes {
+    constructor(init) {
         this.cidr = [];
         this.name = "";
         this.autoSplit = false;
@@ -50,6 +46,6 @@ var Attributes = /** @class */ (function () {
         this.publicSubnets = new SubnetSettings();
         Object.assign(this, init);
     }
-    return Attributes;
-}());
+}
 exports.Attributes = Attributes;
+//# sourceMappingURL=classes.js.map
--- index.js
+++ index.js
@@ -1,50 +1,38 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-exports.__esModule = true;
-exports.createVpc = void 0;
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
 // Import Class definitions from classes.ts, and export these for reference from calling modules.
-var classes_1 = require("./classes");
-__exportStar(require("./classes"), exports);
+const classes_1 = require("./classes");
+__export(require("./classes"));
 // Public Imports
-var aws = require("@pulumi/aws");
-var del_cidr = require("@deloitte-cloud-uk/cidr");
+const aws = require("@pulumi/aws");
+const del_cidr = require("@deloitte-cloud-uk/cidr");
 var DefaultLabels;
 (function (DefaultLabels) {
     DefaultLabels["PublicSunbet"] = "PublicSubnet";
     DefaultLabels["PrivateSubnet"] = "PrivateSubnet";
 })(DefaultLabels || (DefaultLabels = {}));
 function createVpc(attributes) {
-    var response = new classes_1.Response();
+    let response = new classes_1.Response();
     if (attributes.autoSplit) {
         // Pre-Calcuate the Cidr Blocks, because they have not been provided.
         getCidrs(attributes);
     }
     // Create the Main Vpc
-    var vpcResponse = new aws.ec2.Vpc(attributes.name == "" ? "Main" : attributes.name, {
+    const vpcResponse = new aws.ec2.Vpc(attributes.name == "" ? "Main" : attributes.name, {
         cidrBlock: attributes.cidr[0],
         tags: {
             Name: attributes.name
         }
     });
     response.Vpc = vpcResponse;
-    var cidrBlockAssociationResponse;
+    let cidrBlockAssociationResponse;
     // Attach any additional CIDR Blocks
     if (attributes.cidr.length > 1) {
         for (var i = 1; i < attributes.cidr.length; i++) {
-            cidrBlockAssociationResponse = new aws.ec2.VpcIpv4CidrBlockAssociation("Cidr".concat(("00" + String(i)).substr(-2, 2)), {
+            cidrBlockAssociationResponse = new aws.ec2.VpcIpv4CidrBlockAssociation(`Cidr${("00" + String(i)).substr(-2, 2)}`, {
                 vpcId: vpcResponse.id,
                 cidrBlock: attributes.cidr[i]
             });
@@ -52,9 +40,9 @@ function createVpc(attributes) {
         }
     }
     // Create Subnets
-    var subnets = attributes.privateSubnets.subnets.concat(attributes.publicSubnets.subnets);
-    subnets.forEach(function (subnetData) {
-        var subnetResponse = new aws.ec2.Subnet(subnetData.name, {
+    let subnets = attributes.privateSubnets.subnets.concat(attributes.publicSubnets.subnets);
+    subnets.forEach((subnetData) => {
+        let subnetResponse = new aws.ec2.Subnet(subnetData.name, {
             vpcId: vpcResponse.id,
             cidrBlock: subnetData.cidr,
             tags: {
@@ -67,24 +55,25 @@ function createVpc(attributes) {
 }
 exports.createVpc = createVpc;
 function getCidrs(attributes) {
-    var pvt = attributes.privateSubnets;
+    let pvt = attributes.privateSubnets;
     if (pvt.count > 0) {
-        del_cidr.splitIpv4Cidr(pvt.ip(), pvt.mask(), pvt.count).getSubnetList().forEach(function (data, index) {
+        del_cidr.splitIpv4Cidr(pvt.ip(), pvt.mask(), pvt.count).getSubnetList().forEach((data, index) => {
             pvt.subnets.push(new classes_1.Subnet({
                 cidr: data,
-                name: (pvt.prefix == "" ? DefaultLabels.PrivateSubnet : pvt.prefix) + "00".concat(index).substr(-2, 2),
+                name: (pvt.prefix == "" ? DefaultLabels.PrivateSubnet : pvt.prefix) + `00${index}`.substr(-2, 2),
                 type: classes_1.SubnetType.Private
             }));
         });
     }
-    var pub = attributes.publicSubnets;
+    let pub = attributes.publicSubnets;
     if (pub.count > 0) {
-        del_cidr.splitIpv4Cidr(pub.ip(), pub.mask(), pub.count).getSubnetList().forEach(function (data, index) {
+        del_cidr.splitIpv4Cidr(pub.ip(), pub.mask(), pub.count).getSubnetList().forEach((data, index) => {
             pub.subnets.push(new classes_1.Subnet({
                 cidr: data,
-                name: (pvt.prefix == "" ? DefaultLabels.PublicSunbet : pub.prefix) + "00".concat(index).substr(-2, 2),
+                name: (pvt.prefix == "" ? DefaultLabels.PublicSunbet : pub.prefix) + `00${index}`.substr(-2, 2),
                 type: classes_1.SubnetType.Public
             }));
         });
     }
 }
+//# sourceMappingURL=index.js.map
